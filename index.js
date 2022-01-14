c581c5c1e50b8cca8fafd4ff7d4b51627caa79e6
Dev-exponent1 <18-52hl100@students.unilorin.edu.ng>
2022-01-14 12:36:09 +0100

    final commit

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 
 
 var url = "https://api.pexels.com/v1/search?query=searchText";
-var bearerKY = '563492ad6f91700001000001b3fe4d688f73457683854a5b2d60327b'
+var bearer = '563492ad6f91700001000001b3fe4d688f73457683854a5b2d60327b'
 fetch(url, {
         method: 'GET',
-        withCredentials: true,
-        credentials: 'include',
         headers: {
-            'Authorization': `Bearer ${bearerKY}`,
+            'Authorization': bearer,
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '',
-            'Access-Control-Allow-Credentials': 'true'
+            'Access-Control-Allow-Origin': 'http://127.0.0.1:5500',
         }
     }).then(responseJson => {
         var items = JSON.parse(responseJson._bodyInit);
@@ -19,3 +16,11 @@ fetch(url, {
         isLoading: false,
         message: 'Something bad happened ' + error
     }));
+
+
+    // From https://foo.com/
+const response = await fetch('https://api.pexels.com/v1/search?query=searchText', {
+    mode: 'no-cors' // 'cors' by default
+  })
+  console.log(response)
+  // Do something with response
